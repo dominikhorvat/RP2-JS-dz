e254070a1d122dbb7bc40fb9279fe66d6e80d6af
dominikhorvat <dominik.horvat202@gmail.com>
2025-05-30 22:30:07 +0200

    podaci iza igre

---
+++ shikaku - 3.dz/podaci_za_igru.js
@@ -0,0 +1,33 @@
+let game = [];
+
+// ..3.3
+// 2.33.
+// 4....
+// ...2.
+// .23..
+game[0] =
+{
+    name: "First",
+    size: 5,
+    row: [ 1, 1, 2, 2, 2, 3, 4, 5, 5 ],
+    col: [ 3, 5, 1, 3, 4, 1, 4, 2, 3 ],
+    num: [ 3, 3, 2, 3, 3, 4, 2, 2, 3 ],
+    color: ['#daeeff', '#c1d8ec', '#a8c3d9', '#8faec6', '#7799b3', '#5f85a1', '#46718f', '#2a5e7e', '#004c6d' ]
+};
+
+
+// aAAAAA
+// AAAAAA
+// BBBCCD
+// BBBCcd 
+// BBBEeE
+// bBBEEE
+game[1] =
+{
+    name: "Second",
+    size: 6,
+    row: [  1,  6, 4, 4, 5 ],
+    col: [  1,  1, 5, 6, 5 ],
+    num: [ 12, 12, 4, 2, 6 ],
+    color: ['yellow', 'blue', 'green', 'pink', 'gray']
+};
